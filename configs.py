a38b12ad3436d08c753458cdfb66dbde09621116
Gino Pak <ginopak1403@gmail.com>
2025-11-05 20:07:59 -0500

    adding configs.py file

---
+++ configs.py
@@ -0,0 +1,10 @@
+LLM_API_KEY = """Add your own key here"""
+LLM_BASE_URL = """Add your own key here"""
+MODEL = """Add your own model here"""
+
+SYNTHETIC_LISTING_CSV_PATH = """Add your path key here"""
+
+CLEANED_LISTING_CSV_PATH = """Add your path key here"""
+
+MERGED_LISTING_CSV_PATH = """Add your path key here"""
+USERS_DATA_FILE = """Add your own path here"""
